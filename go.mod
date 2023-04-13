c784427bad3a607c714554e3ba8ffa4805d39e39
baoyx <byx19870908@yeah.net>
2023-04-13 23:13:20 +0800

    feat:add media m3u8 tool

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/grafov/m3u8 v0.12.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 )
---
+++ utils/media.go
@@ -0,0 +1,83 @@
+package utils
+
+import (
+	"bytes"
+	"context"
+	"github.com/baoyxing/go-tools/net/fasthttp"
+	"github.com/grafov/m3u8"
+	"github.com/grafov/m3u8/example/template"
+	nUrl "net/url"
+	"time"
+)
+
+type MediaPlaylist struct {
+	TargetDuration float64
+	SeqNo          uint64 // EXT-X-MEDIA-SEQUENCE
+	Segments       []*MediaSegment
+	Ver            uint8
+	Count          uint
+	MediaLength    uint64
+}
+
+type MediaSegment struct {
+	Size            uint64
+	Url             string
+	Duration        float64
+	ProgramDateTime time.Time
+	Start           uint64
+	End             uint64
+}
+
+func ParseM3u8Ts(ctx context.Context, url string) (*MediaPlaylist, error) {
+	body, err := fasthttp.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	customTags := []m3u8.CustomDecoder{
+		&template.CustomPlaylistTag{},
+		&template.CustomSegmentTag{},
+	}
+	p, listType, err := m3u8.DecodeWith(*bytes.NewBuffer(body), false, customTags)
+	if err != nil {
+		return nil, err
+	}
+	switch listType {
+	case m3u8.MEDIA:
+		noSuffixUrl := GetNoFileUrl(url)
+		_, err := nUrl.Parse(noSuffixUrl)
+		if err != nil {
+			return nil, err
+		}
+		mediapl := p.(*m3u8.MediaPlaylist)
+		count := mediapl.Count()
+		mediaPlaylist := &MediaPlaylist{
+			TargetDuration: mediapl.TargetDuration,
+			SeqNo:          mediapl.SeqNo,
+			Segments:       make([]*MediaSegment, 0, mediapl.Count()),
+			Ver:            mediapl.Version(),
+			Count:          count,
+			MediaLength:    0,
+		}
+		for _, segment := range mediapl.Segments {
+			if segment != nil {
+				mediaSegment := &MediaSegment{
+					Url:             segment.URI,
+					Duration:        segment.Duration,
+					ProgramDateTime: segment.ProgramDateTime,
+				}
+				mediaPlaylist.Segments = append(mediaPlaylist.Segments, mediaSegment)
+			}
+		}
+		return mediaPlaylist, nil
+	case m3u8.MASTER:
+		noSuffixUrl := GetNoSuffixUrl(url)
+		masterpl := p.(*m3u8.MasterPlaylist)
+		_url, err := nUrl.Parse(noSuffixUrl)
+		if err != nil {
+			return nil, err
+		}
+		url = ResolveURL(_url, masterpl.Variants[0].URI)
+		return ParseM3u8Ts(ctx, url)
+	}
+	return nil, nil
+}
