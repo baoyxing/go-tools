c3cc1ab12ac0dfd2b8729005bb976b3bf00f13a2
baoyx <byx19870908@yeah.net>
2023-05-11 08:49:28 +0800

    feat add md5 encrpt

--- utils/encrypt.go
+++ utils/encrypt.go
@@ -1,12 +1,18 @@
 package utils
 
 import (
+	"crypto/md5"
 	"crypto/sha1"
 	"encoding/hex"
+	"fmt"
 )
 
-func Hash1(content string) string {
+func Hash1(content []byte) string {
 	h := sha1.New()
-	h.Write([]byte(content))
+	h.Write(content)
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func MD5(content []byte) string {
+	return fmt.Sprintf("%x", md5.Sum(content))
 }
